c49dabfb6c5c43c286bf408cdb94493f04d35f51
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-02-08 13:18:46 +0530

    TF code for Subnets under VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,3 +4,13 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-${var.project_name}-vpc"
   }
 }
+
+resource "aws_subnet" "main" {
+  count      = length(var.subnets_cidr)  #we provided 2 values in the subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.subnets_cidr,count.index)  # we are calling each value in subnets-cidr
+
+  tags = {
+    Name = "subnet-${count.index}"
+  }
+}
