ae4987d2e93197f379c04b6b1263ed9ecf42a690
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-02-09 17:35:26 +0530

    TF code for creating route tables for private and public subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -24,8 +24,8 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
   count = length(var.public_subnets_cidr)
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id  # Here we are attaching internet Gateway
@@ -46,8 +46,8 @@ resource "aws_subnet" "private" {
   }
 }
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
   count = length(var.private_subnets_cidr)
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id  # Here we are attaching internet Gateway
