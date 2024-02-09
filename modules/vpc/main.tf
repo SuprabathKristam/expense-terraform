25f2f1feeeedfcf0f084bca456c3f1d7c29c6de9
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-02-09 15:34:43 +0530

    TF code for creating private and public subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public" {
   availability_zone = element(var.az, count.index)
 
   tags = {
-    Name = "subnet-${count.index}"
+    Name = "public-subnet-${count.index+1}"
   }
 }
 
@@ -23,7 +23,7 @@ resource "aws_subnet" "private" {
   availability_zone = element(var.az, count.index)
 
   tags = {
-    Name = "subnet-${count.index}"
+    Name = "private-subnet-${count.index+1}"
   }
 }
 
