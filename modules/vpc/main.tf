4c95e6cfa10c52b8f29e8646c8e82968937fafc3
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-02-09 18:06:36 +0530

    TF code for mapping of newly created route tables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -51,7 +51,7 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public" {  #Used for mapping of newly created route tables
   count          = length(var.public_subnets_cidr)
-  route_table_id = lookup(element(aws_route_table.public.id, count.index),"id", null) #aws_route_table.public[count.index].id
+  route_table_id = lookup(element(aws_route_table.public, count.index), "id", null) #aws_route_table.public[count.index].id
   subnet_id      = lookup(element(aws_subnet.public, count.index),"id", null )
 }
 
