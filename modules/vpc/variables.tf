4b92db7fc936feef544522ea401b0153bb254a73
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-02-10 22:15:02 +0530

    TF code to organise all 3 layers of the application

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -3,4 +3,6 @@ variable "env" {}
 variable "project_name" {}
 variable "az" {}
 variable "public_subnets_cidr" {}
-variable "private_subnets_cidr" {}
+variable "web_subnets_cidr" {}
+variable "app_subnets_cidr" {}
+variable "db_subnets_cidr" {}
