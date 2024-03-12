5198ebc149e8d91b6ae963f3af229f300a045844
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-03-12 18:38:18 +0530

    TF code link load balancers with subnets

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "backend" {
   instance_capacity           = var.backend_instance_capacity
   instance_type               = var.backend_instance_type
   project_name                = var.project_name
-  sg_cidr_blocks              = var.web_subnets_cidr # as the apps should be accessible to web subnets
+  sg_cidr_blocks              = var.app_subnets_cidr
   vpc_id                      = module.vpc.vpc_id
   vpc_zone_identifier         = module.vpc.app_subnet_ids
 }
@@ -85,7 +85,7 @@ module "frontend" {
   instance_capacity           = var.frontend_instance_capacity
   instance_type               = var.frontend_instance_type
   project_name                = var.project_name
-  sg_cidr_blocks              = var.web_subnets_cidr # this needs to be checked
+  sg_cidr_blocks              = var.public_subnets_cidr # this needs to be checked
   vpc_id                      = module.vpc.vpc_id
   vpc_zone_identifier         = module.vpc.web_subnet_ids
 }
