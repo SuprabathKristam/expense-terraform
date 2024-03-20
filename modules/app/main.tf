506ae7c8e8e447664b21783b9a125da230be0a88
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-03-20 19:35:11 +0530

    TF code for attaching instance profiles to instances by launch template

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -35,6 +35,9 @@ resource "aws_launch_template" "main" {
   image_id      = data.aws_ami.centos-8.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  iam_instance_profile {
+    name = aws_iam_role.main.name
+  }
 }
 
 resource "aws_autoscaling_group" "main" {
